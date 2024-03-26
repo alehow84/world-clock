4896d3804e0f8e3f590472d2f94ad457a809226c
Alexandra Howlett <alexandrahowlett@Alexandras-MacBook-Pro.local>
2024-03-26 15:02:04 +0000

    replace #cities div with selected city time and date info when user chooses from the select - background styling needs fixing

--- javascript/index.js
+++ javascript/index.js
@@ -1,21 +1,6 @@
-/*
-Purpose - Add functionality so that when the user selects a city from the dropdown, the world clock is populated with just one city and its time/date information.
-
-- add values to the options in the select that are the moment timezones for each city DONE
--define a function that is called by an event listener 
--this function will grab the value of the event and define it as a variable called cutyTimezone
-- 
-- add an event listener to the city-select
-
-<span class="fi fi-jp fis"></span>
-
-<span class="fi fi-dk fis"></span>
-
-<span class="fi fi-me fis"></span>
-
-<span class="fi fi-is fis"></span>
-
-*/
+let cityOneElement = document.querySelector("#toronto");
+let citiesElement = document.querySelector("#cities");
+let timezoneContainer = "";
 
 function updateTime() {
   //Toronto
@@ -68,16 +53,84 @@ function updateTime() {
     honoluluElementTime.innerHTML = honoluluElementTimezone.format(
       "HH:mm:ss [<small>]A[</small>]"
     );
+
+    // let singleCityElement = document.querySelector("#single-city");
+    // if (singleCityElement) {
+    //   let singleCityElementDate = singleCityElement.querySelector(".date");
+    //   let singleCityElementTime = singleCityElement.querySelector(".time");
+    //   let singleCityElementTimezone = moment().tz(`${timezoneContainer}`);
+    //   singleCityElementDate.innerHTML =
+    //     singleCityElementTimezone.format("ddd Do MMMM YYYY");
+    //   singleCityElementTime.innerHTML = singleCityElementTimezone.format(
+    //     "HH:mm:ss [<small>]A[</small>]"
+    //   );
+    // }
   }
 }
 
-updateTime();
+function changeCity(event) {
+  let flagRef = "";
+  timezoneContainer = event.target.value;
+  if (timezoneContainer === "Asia/Tokyo") {
+    flagRef = "jp";
+  } else if (timezoneContainer === "Europe/Copenhagen") {
+    flagRef = "dk";
+  } else if (timezoneContainer === "Europe/Budapest") {
+    flagRef = "hu";
+  } else if (timezoneContainer === "Atlantic/Reykjavik") {
+    flagRef = "is";
+  }
+  let selectedCityName = timezoneContainer.split("/")[1];
+  console.log(selectedCityName, "selectedCityName");
 
-setInterval(updateTime, 1000);
+  let cityDateInfo = moment().tz(`${timezoneContainer}`);
 
-function changeCity() {
-  let cityTimezone = event.target.value;
+  citiesElement.innerHTML = `<div class="city">
+<div>
+  <h2>${selectedCityName} <span class="fi fi-${flagRef} fis"></span></h2>
+  <div class="date">${cityDateInfo.format("ddd Do MMMM YYYY")}</div>
+</div>
+<div class="time">${cityDateInfo.format("HH:mm:ss [<small>]A[</small>]")}</div>
+</div>`;
 }
 
-let citySelect = document.querySelector(".city-select");
-citySelect.addEventListener();
+function resetSelect() {
+  citiesElement.innerHTML = `<div class="city" id="toronto">
+<div>
+  <h2>Toronto <span class="fi fi-ca fis"></span></h2>
+  <div class="date"></div>
+</div>
+<div class="time"></div>
+</div>
+<div class="city" id="norwich">
+<div>
+  <h2>Norwich <span class="fi fi-gb fis"></span></h2>
+  <div class="date"></div>
+</div>
+<div class="time"></div>
+</div>
+<div class="city" id="berlin">
+<div>
+  <h2>Berlin <span class="fi fi-de fis"></span></h2>
+  <div class="date"></div>
+</div>
+<div class="time"></div>
+</div>
+<div class="city" id="honolulu">
+<div>
+  <h2>Honolulu <span class="fi fi-us fis"></span></h2>
+  <div class="date"></div>
+</div>
+<div class="time"></div>
+</div>
+</div>`;
+}
+
+let citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", changeCity);
+
+let cityReset = document.querySelector("#reset");
+cityReset.addEventListener("click", resetSelect);
+
+updateTime();
+setInterval(updateTime, 1000);
