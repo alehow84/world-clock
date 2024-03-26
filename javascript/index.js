8b7053836fd00cb9e7a8d7ae15be14f287a788ce
Alexandra Howlett <alexandrahowlett@Alexandras-MacBook-Pro.local>
2024-03-26 11:24:55 +0000

    refactor updateTime function in preparation for changeCity function implementation

--- javascript/index.js
+++ javascript/index.js
@@ -1,47 +1,83 @@
-const updateTime = () => {
+/*
+Purpose - Add functionality so that when the user selects a city from the dropdown, the world clock is populated with just one city and its time/date information.
+
+- add values to the options in the select that are the moment timezones for each city DONE
+-define a function that is called by an event listener 
+-this function will grab the value of the event and define it as a variable called cutyTimezone
+- 
+- add an event listener to the city-select
+
+<span class="fi fi-jp fis"></span>
+
+<span class="fi fi-dk fis"></span>
+
+<span class="fi fi-me fis"></span>
+
+<span class="fi fi-is fis"></span>
+
+*/
+
+function updateTime() {
   //Toronto
-  let mainElement = document.querySelector("#city-main");
-  let mainElementDate = mainElement.querySelector(".date");
-  let mainElementTime = mainElement.querySelector(".time");
-  let mainElementTimezone = moment().tz("America/Toronto");
-  mainElementDate.innerHTML = mainElementTimezone.format("ddd Do MMMM YYYY");
-  mainElementTime.innerHTML = mainElementTimezone.format(
-    "HH:mm:ss [<small>]A[</small>]"
-  );
+  let torontoElement = document.querySelector("#toronto");
+  if (torontoElement) {
+    let torontoElementDate = torontoElement.querySelector(".date");
+    let torontoElementTime = torontoElement.querySelector(".time");
+    let torontoElementTimezone = moment().tz("America/Toronto");
+    torontoElementDate.innerHTML =
+      torontoElementTimezone.format("ddd Do MMMM YYYY");
+    torontoElementTime.innerHTML = torontoElementTimezone.format(
+      "HH:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Norwich
-  let secondElement = document.querySelector("#city-2");
-  let secondElementDate = secondElement.querySelector(".date");
-  let secondElementTime = secondElement.querySelector(".time");
-  let secondElementTimezone = moment().tz("Europe/London");
-  secondElementDate.innerHTML =
-    secondElementTimezone.format("ddd Do MMMM YYYY");
-  secondElementTime.innerHTML = secondElementTimezone.format(
-    "HH:mm:ss [<small>]A[</small>]"
-  );
+  let norwichElement = document.querySelector("#norwich");
+  if (norwichElement) {
+    let norwichElementDate = norwichElement.querySelector(".date");
+    let norwichElementTime = norwichElement.querySelector(".time");
+    let norwichElementTimezone = moment().tz("Europe/London");
+    norwichElementDate.innerHTML =
+      norwichElementTimezone.format("ddd Do MMMM YYYY");
+    norwichElementTime.innerHTML = norwichElementTimezone.format(
+      "HH:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Berlin
-  let thirdElement = document.querySelector("#city-3");
-  let thirdElementDate = thirdElement.querySelector(".date");
-  let thirdElementTime = thirdElement.querySelector(".time");
-  let thirdElementTimezone = moment().tz("Europe/London");
-  thirdElementDate.innerHTML = thirdElementTimezone.format("ddd Do MMMM YYYY");
-  thirdElementTime.innerHTML = thirdElementTimezone.format(
-    "HH:mm:ss [<small>]A[</small>]"
-  );
+  let berlinElement = document.querySelector("#berlin");
+  if (berlinElement) {
+    let berlinElementDate = berlinElement.querySelector(".date");
+    let berlinElementTime = berlinElement.querySelector(".time");
+    let berlinElementTimezone = moment().tz("Europe/Berlin");
+    berlinElementDate.innerHTML =
+      berlinElementTimezone.format("ddd Do MMMM YYYY");
+    berlinElementTime.innerHTML = berlinElementTimezone.format(
+      "HH:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   //Honolulu
-  let fourthElement = document.querySelector("#city-4");
-  let fourthElementDate = fourthElement.querySelector(".date");
-  let fourthElementTime = fourthElement.querySelector(".time");
-  let fourthElementTimezone = moment().tz("Pacific/Honolulu");
-  fourthElementDate.innerHTML =
-    fourthElementTimezone.format("ddd Do MMMM YYYY");
-  fourthElementTime.innerHTML = fourthElementTimezone.format(
-    "HH:mm:ss [<small>]A[</small>]"
-  );
-};
+  let honoluluElement = document.querySelector("#honolulu");
+  if (honoluluElement) {
+    let honoluluElementDate = honoluluElement.querySelector(".date");
+    let honoluluElementTime = honoluluElement.querySelector(".time");
+    let honoluluElementTimezone = moment().tz("Pacific/Honolulu");
+    honoluluElementDate.innerHTML =
+      honoluluElementTimezone.format("ddd Do MMMM YYYY");
+    honoluluElementTime.innerHTML = honoluluElementTimezone.format(
+      "HH:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
 
 updateTime();
 
 setInterval(updateTime, 1000);
+
+function changeCity() {
+  let cityTimezone = event.target.value;
+}
+
+let citySelect = document.querySelector(".city-select");
+citySelect.addEventListener();
